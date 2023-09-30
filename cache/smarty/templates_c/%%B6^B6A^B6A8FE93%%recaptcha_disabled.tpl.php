c51a229cb404d5fb493bf7e9d5eb512750bf95c4
mohammed awadh <mohammed.n.awadh@gmail.com>
2023-09-30 15:10:45 -0500

    new commit

---
+++ cache/smarty/templates_c/%%B6^B6A^B6A8FE93%%recaptcha_disabled.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version 2.6.31, created on 2023-09-30 14:26:20
+         compiled from /var/www/html/crm/include/utils/recaptcha_disabled.tpl */ ?>
+<?php echo '
+<script>
+
+  /**
+   * Login Screen Validation
+   */
+  function validateAndSubmit() {
+      generatepwd();
+    }
+
+  /**
+   * Password reset screen validation
+   */
+  function validateCaptchaAndSubmit() {
+      document.getElementById(\'username_password\').value = document.getElementById(\'new_password\').value;
+      document.getElementById(\'ChangePasswordForm\').submit();
+    }
+</script>
+'; ?>
